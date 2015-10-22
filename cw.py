93d94b592fc196fcffe8bf04496cacdece4d5562
Dimitrios Ntoulas <ntoulasd@yahoo.gr>
2015-10-22 21:38:32 +0300

    Sound freq fix

--- cw.py
+++ cw.py
@@ -19,14 +19,14 @@ if __name__ == '__main__':
                         print ". ";
                         ser.setRTS(0)
                         #time.sleep (dot)
-                        os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( dot, 800 ))
+                        os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( dot, 500 ))
                         ser.setRTS(1)
                         time.sleep (dot/2)
                 if c =='/':
                         print "_ ";
                         ser.setRTS(0)
                         #time.sleep (3*dot)
-                        os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 3*dot, 800 ))
+                        os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % ( 3*dot, 500 ))
                         ser.setRTS(1)
                         time.sleep (dot/2)
                 if c =='z':
